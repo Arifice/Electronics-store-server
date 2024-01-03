37e0ec3c632fa4783323ff34d316c273912a2109
Ariful Islam <arifice13@gmail.com>
2024-01-03 18:32:01 +0600

    product create, update, delete,read operation done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const  express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -30,12 +30,69 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection=client.db('productDB').collection('product');
+
+    // create product    
+    app.post('/product',async(req,res)=>{
+      const newProduct=req.body;
+      console.log(newProduct);
+      const result=await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    // read or get product
+    app.get('/product',async(req,res)=>{
+      const cursor=productCollection.find();
+      const result=await cursor.toArray();
+      res.send(result);
+    })
+
+
+    // update product
+    app.get('/product/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result=await productCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.put('/product/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id: new ObjectId(id)};
+      const options={ upsert: true};
+      const updatedProduct=req.body;
+      const product={
+        $set:{
+          productName:updatedProduct.productName,
+          image:updatedProduct.image,
+          brandName:updatedProduct.brandName,
+          type:updatedProduct.type,
+          price:updatedProduct.price,
+          description:updatedProduct.description
+        }
+      }
+      const result=await productCollection.updateOne(filter,product,options);
+      res.send(result);
+    })
+
+
+    // delete product
+    app.delete('/product/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result=await productCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // 
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
